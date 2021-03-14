9c611e595e917c8e890d8100fe7873d6f7ca95a7
denismark94 <denismark94@gmail.com>
2021-03-14 20:54:18 +0300

    feat: implement brackets checker

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var count = 0;
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if ((bracketsConfig[j][0] != bracketsConfig[j][1]) && (str[i] == bracketsConfig[j][1])) {return false;};
+      if (str[i] == bracketsConfig[j][0]) {
+        count ++;
+        for (let k = i+1; k < str.length; k++) {
+          if (str[k] == bracketsConfig[j][1]) {count --;};
+          if (bracketsConfig[j][0] != bracketsConfig[j][1]) {
+            if (str[k] == bracketsConfig[j][0]) {count ++;};
+          };
+          if (count == 0) {
+            var a = check(str.substring(i+1,k),bracketsConfig) 
+            var b = check(str.substring(k+1),bracketsConfig);
+            return a && b 
+          }
+        }
+      }
+    }    
+  }
+  return str.length == 0;
 }
